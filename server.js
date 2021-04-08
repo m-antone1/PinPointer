20c336ddb171acc523c1685ae2879bbf8b5fdf79
Michael Antone <mike_a1@live.com>
2021-04-08 23:08:38 +0000

    WIP fixing login route

--- server.js
+++ server.js
@@ -72,29 +72,31 @@ app.get("/", (req, res) => {
 });
 
 //post for login, and e-mail and password check
-app.post("/login", (req, res) => {
+app.get("/login:id", (req, res) => {
   console.log("Test", req.body);
-  const loginEmail = req.body.email;
-  const loginPass = req.body.loginPass;
-  if (loginEmail === "" && loginPass === undefined) {
-    res.status(403).send("Please use a valid email");
-    return;
-  }
-  if (loginEmail === undefined && loginPass === "") {
-    res.status(403).send("Please use a valid password");
-    return;
-  }
+  res.cookie('user_id', req.params.id);
+  res.redirect('/');
+  // const loginEmail = req.body.email;
+  // const loginPass = req.body.loginPass;
+  // if (loginEmail === "" && loginPass === undefined) {
+  //   res.status(403).send("Please use a valid email");
+  //   return;
+  // }
+  // if (loginEmail === undefined && loginPass === "") {
+  //   res.status(403).send("Please use a valid password");
+  //   return;
+  // }
 
   //retrieving from db for users
-  let query = `SELECT * FROM users;`;
-  db.query(query)
-    .then(data => {
-      formHandling(req, res, data.rows);
-      return;
-    })
-    .catch(err => {
-      res.status(500).json({ error: err.message });
-    });
+  // let query = `SELECT * FROM users;`;
+  // db.query(query)
+  //   .then(data => {
+  //     formHandling(req, res, data.rows);
+  //     return;
+  //   })
+  //   .catch(err => {
+  //     res.status(500).json({ error: err.message });
+  //   });
 });
 
 //clearing cookies after logout
